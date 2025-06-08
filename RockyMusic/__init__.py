232c1b837d1e4012d31241aa39492c6e00e446de
abh628 <rockybhaiowner@gmail.com>
2025-06-09 00:31:17 +0530

    Add files via upload

---
+++ RockyMusic/__init__.py
@@ -0,0 +1,26 @@
+from RockyMusic.core.bot import Aviax
+from RockyMusic.core.dir import dirr
+from RockyMusic.core.git import git
+from RockyMusic.core.userbot import Userbot
+from RockyMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Aviax()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
---
+++ RockyMusic/utils/inline/start.py
@@ -0,0 +1,41 @@
+from pyrogram.types import InlineKeyboardButton
+import config
+from RockyMusic import app
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+        ],
+        [
+            InlineKeyboardButton(text=_["E_X_1"], url="https://github.com/NoxxOP/ShrutiMusic/fork"),
+            InlineKeyboardButton(text="˹𝐏ʀɪᴠᴀᴄʏ 𝐏ᴏʟɪᴄʏ˼", url="https://graph.org/Privacy-Policy-05-01-30")
+        ],
+    ]
+    return buttons
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(text=_["E_X_1"], url="https://github.com/NoxxOP/ShrutiMusic/fork"),
+            InlineKeyboardButton(text="˹𝐏ʀɪᴠᴀᴄʏ 𝐏ᴏʟɪᴄʏ˼", url="https://graph.org/Privacy-Policy-05-01-30")
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+        ],
+    ]
+    return buttons
