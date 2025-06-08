f95bf38fd35be5c925252fa129c5963fc3ac4aee
abh628 <rockybhaiowner@gmail.com>
2025-06-09 01:06:27 +0530

    Update and rename Youtube (1).py to Youtube.py

--- RockyMusic/platforms/Youtube.py
+++ RockyMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from AloneX.utils.database import is_on_off
-from AloneX.utils.formatters import time_to_seconds
+from RockyMusic.utils.database import is_on_off
+from RockyMusic.utils.formatters import time_to_seconds
 import os
 import glob
 import random
