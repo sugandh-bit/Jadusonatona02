4579b1911da064d00d2d3b4003e4d111a7bb1918
abh628 <rockybhaiowner@gmail.com>
2025-06-13 04:06:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ load_dotenv()
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID", None))
 OWNER_USERNAME = os.getenv("OWNER_USERNAME", "Mr_Rocky_Tz")
 
